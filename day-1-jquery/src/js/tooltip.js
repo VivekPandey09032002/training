9f296c18c0df4bb4211be3f135926dc791decae4
VivekPandey09032002 <vivekpandeykcc09032002@gmail.com>
2023-08-07 16:46:32 +0530

    nav bar added

--- day-1-jquery/src/js/tooltip.js
+++ day-1-jquery/src/js/tooltip.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
   $('[data-bs-toggle="tooltip"]').tooltip({
     html: true,
   });
+  const elementToToggle = $("#nav-bar");
+  $("#nav-toogle").on("click", function () {
+    if (elementToToggle.css("display") === "none") {
+      elementToToggle.show();
+    } else {
+      elementToToggle.hide();
+    }
+  });
 });
